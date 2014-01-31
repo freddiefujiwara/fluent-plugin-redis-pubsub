8c5cc13aeeab38e2066d84281d589a4a14502cfc
Victor Castell <victor@victorcastell.com>
2014-01-31 16:27:55 +0100

    Ruby style indent

--- lib/fluent/plugin/out_redis_pubsub.rb
+++ lib/fluent/plugin/out_redis_pubsub.rb
@@ -1,43 +1,43 @@
 module Fluent
-    class RedisPubsubOutput < BufferedOutput
-        Plugin.register_output('redis_pubsub', self)
-        attr_reader :host, :port, :channel, :redis
+  class RedisPubsubOutput < BufferedOutput
+    Plugin.register_output('redis_pubsub', self)
+    attr_reader :host, :port, :channel, :redis
 
-        def initialize
-            super
-            require 'redis'
-            require 'msgpack'
-        end
-
-        def configure(config)
-            super
-            @host    = config.has_key?('host')    ? config['host']         : 'localhost'
-            @port    = config.has_key?('port')    ? config['port'].to_i    : 6379
-            raise Fluent::ConfigError, "need channel" if not config.has_key?('channel') or config['channel'].empty?
-            @channel = config['channel'].to_s
-        end
-
-        def start
-            super
-            @redis = Redis.new(:host => @host, :port => @port ,:thread_safe => true)
-        end
-
-        def shutdown
-            @redis.quit
-        end
-
-        def format(tag, time, record)
-            record['__tag__']  = tag
-            record['__time__'] = time
-            record.to_msgpack
-        end
-
-        def write(chunk)
-            @redis.pipelined do
-                chunk.msgpack_each do |record|
-                    @redis.publish @channel, record.to_json
-                end
-            end
-        end
+    def initialize
+      super
+      require 'redis'
+      require 'msgpack'
     end
+
+    def configure(config)
+      super
+      @host    = config.has_key?('host')    ? config['host']         : 'localhost'
+      @port    = config.has_key?('port')    ? config['port'].to_i    : 6379
+      raise Fluent::ConfigError, "need channel" if not config.has_key?('channel') or config['channel'].empty?
+      @channel = config['channel'].to_s
+    end
+
+    def start
+      super
+      @redis = Redis.new(:host => @host, :port => @port ,:thread_safe => true)
+    end
+
+    def shutdown
+      @redis.quit
+    end
+
+    def format(tag, time, record)
+      record['__tag__']  = tag
+      record['__time__'] = time
+      record.to_msgpack
+    end
+
+    def write(chunk)
+      @redis.pipelined do
+        chunk.msgpack_each do |record|
+          @redis.publish @channel, record.to_json
+        end
+      end
+    end
+  end
 end
